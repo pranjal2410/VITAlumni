522a107d9ae56d43799cebb57f03e03150b2c459
unknown <newalkarpranjal2410.pn@gmail.com>
2021-01-28 00:32:29 +0530

    Check

--- VITAlumni/settings.py
+++ VITAlumni/settings.py
@@ -179,8 +179,8 @@ JWT_AUTH = {
 }
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 EMAIL_HOST_USER = 'alumni.vit18@gmail.com'
 EMAIL_HOST_PASSWORD = 'Root1234'
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
